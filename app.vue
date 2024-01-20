5345706f104a7adf73eaee9bf595572fa53cd19f
John Michael Malbas <923415@ACDM.DS.SAIT.CA>
2024-01-19 23:29:55 -0700

    Added app.vue and nuxt.config.ts a value

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="app">
+    <AppHeader />
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+/* Add this style to center the image */
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px; /* Adjust the height as needed */
+}
+body,
+.app {
+  margin: 0;
+}
+</style>
