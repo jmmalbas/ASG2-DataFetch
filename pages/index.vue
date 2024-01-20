d63c208fa748786d9c88d27a5ef83a8c460ea38d
John Michael Malbas <923415@ACDM.DS.SAIT.CA>
2024-01-20 00:09:25 -0700

    Added custom background color and fonts

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@ const characters = data;
 </script>
 
 <template>
-  <main>
+  <main class="main-container">
     <header>
-      <h1>DISNEY CHARACTERS</h1>
+      <strong><h1 class="page-title">DISNEY CHARACTERS</h1></strong>
     </header>
 
     <section>
@@ -19,10 +19,32 @@ const characters = data;
         </li>
       </ul>
     </section>
+    <footer>
+      <p class="footer-text">2024 John Michael Malbas</p>
+    </footer>
   </main>
 </template>
 
 <style>
+.footer-text {
+  background-color: black;
+  text-align: center;
+  margin-top: 20px;
+  color: white; 
+}
+.main-container {
+  background-color: rgb(83, 83, 83); 
+  color: white; 
+  min-height: 100vh; 
+}
+
+.page-title {
+  text-align: center;
+  background-color: black;
+  color: white;
+  padding: 20px;
+}
+
 .characters-list {
   list-style: none;
   padding: 0;
@@ -43,7 +65,9 @@ const characters = data;
 }
 
 .character-container img {
-  margin-top: 10px; 
+  margin-top: 10; 
 }
+
+
 </style>
 
